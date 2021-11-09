96f6f26bdaf7585306f0461c4e0ade73d312f4fd
Bryce <91035218+BriccsMe@users.noreply.github.com>
2021-11-09 09:40:04 -0600

    Version 0.1

---
+++ Liminal01.java
@@ -0,0 +1,34 @@
+package Liminal;
+import java.util.Scanner;
+
+public class Liminal01 
+{
+	public static void main(String[] args)
+	{
+		int a; int b; int c; int d; int e; int f; int running=1;
+		Scanner in = new Scanner(System.in);
+		System.out.println("\tWelcome to the Borely Haunted Mansion.");
+		System.out.println("\t\t\tPlay? (Y)");
+		
+		while (running==1)
+		{
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
